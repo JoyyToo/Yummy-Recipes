73e900c0a086cab64844eb6af29cd4842857e868
JoyToo <you@example.com>
2017-10-30 00:54:33 +0300

    [chore] add more tests

--- tests/test_for_class_category.py
+++ tests/test_for_class_category.py
@@ -12,18 +12,23 @@ class TestForClassCategory(unittest.TestCase):
 
     def test_for_empty_fields(self):
         """Test for empty fields"""
-        result = self.cat.create_category('', '', '')
+        result = self.cat.create_category('', '', '', '')
         self.assertEqual({"status": "error", "message": "Fill all the fields"}, result)
 
     def test_for_empty_name_field(self):
         """Test for empty name field"""
-        result = self.cat.create_category('', 'after dinner', 1)
+        result = self.cat.create_category('', 'after dinner', 1, 'file.png')
         self.assertEqual({"status": "error", "message": "Fill all the fields"}, result)
 
     def test_for_empty_description_field(self):
         """Test for empty description field"""
-        result = self.cat.create_category('dessert', '', 1)
+        result = self.cat.create_category('dessert', '', 1, 'file.png')
         self.assertEqual({"status": "error", "message": "Fill all the fields"}, result)
+
+    def test_for_empty_image_url_field(self):
+        """Test for empty image url field"""
+        result = self.cat.create_category('dessert', 'after dinner', 1, '')
+        self.assertEqual({"status": "error", "message": 'No file chosen'}, result)
 
     def test_for_non_existing_category(self):
         """Test for non-existing category"""
@@ -33,8 +38,8 @@ class TestForClassCategory(unittest.TestCase):
 
     def test_for_missing_update_category(self):
         """Test for non-existing update category"""
-        self.cat.update_category(1, 'Appetizers', 'a desc', 1)
-        result = self.cat.update_category(2, 'App', 'available before', 1)
+        self.cat.update_category(1, 'Appetizers', 'a desc', 1, 'file.png')
+        result = self.cat.update_category(2, 'App', 'available before', 1, 'file.png')
         self.assertEqual({"status": "error", "message": "Category does not exist"}, result)
 
     def test_for_delete_missing_category(self):
@@ -42,3 +47,14 @@ class TestForClassCategory(unittest.TestCase):
         self.cat.delete_category(2)
         result = self.cat.delete_category(0)
         self.assertEqual({"status": "error", "message": "Sorry, Category does not exist"}, result)
+
+    def test_for_display_single_non_existing_category(self):
+        """Test for non-existing display single category"""
+        self.cat.single_category(0)
+        result = self.cat.single_category(2)
+        self.assertEqual({"status": "error", "message": "Category not available"}, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
+

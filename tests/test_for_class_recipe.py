876ed4b4232260169231f1d72f248d3136b77d0e
JoyToo <you@example.com>
2017-11-02 08:41:20 +0300

    [chore]add more tests

--- tests/test_for_class_recipe.py
+++ tests/test_for_class_recipe.py
@@ -1,76 +1,134 @@
 """Test for Recipe class"""
 
+import os
 import unittest
 from app.models.Recipe import Recipe
+from PIL import Image
 
 
 class TestForClassRecipe(unittest.TestCase):
     """Test for recipe class"""
     def setUp(self):
         """Initializes test for recipe Class"""
-        self.rec = Recipe()
+        self.recipe = Recipe()
+        self.recipe.allrecipes = {}
 
     # tests for add recipe function
     def test_for_empty_fields(self):
         """Test for empty fields"""
-        result = self.rec.create_recipe('', '', '', '', 0, 0, 'file.png')
+        result = self.recipe.create_recipe('', '', '', '', 0, 0, 'file.png')
         self.assertEqual({"status": "error", "message": 'Fill all fields'}, result)
 
     def test_for_empty_name_field(self):
         """Test for empty name field"""
-        result = self.rec.create_recipe('', '1 hour', ' 1 tbsp ', 'stir', 1, 2, 'file.png')
+        result = self.recipe.create_recipe('', '1 hour', ' 1 tbsp ', 'stir', 1, 2, 'file.png')
         self.assertEqual({"status": "error", "message": 'Fill all fields'}, result)
 
     def test_for_empty_time_field(self):
         """Test for empty time field"""
-        result = self.rec.create_recipe('biryani', '', '1 tbsp', 'stir', 1, 2, 'file.png')
+        result = self.recipe.create_recipe('biryani', '', '1 tbsp', 'stir', 1, 2, 'file.png')
         self.assertEqual({"status": "error", "message": 'Fill all fields'}, result)
 
     def test_for_empty_ingredients_field(self):
         """Test for empty ingredients field"""
-        result = self.rec.create_recipe('biryani', '1 hour', '', 'stir', 1, 2, 'file.png')
+        result = self.recipe.create_recipe('biryani', '1 hour', '', 'stir', 1, 2, 'file.png')
         self.assertEqual({"status": "error", "message": 'Fill all fields'}, result)
 
     def test_for_empty_direction_field(self):
         """Test for empty direction field"""
-        result = self.rec.create_recipe('biryani', '1 hour', '1 tbsp', '', 1, 2, 'file.png')
+        result = self.recipe.create_recipe('biryani', '1 hour', '1 tbsp', '', 1, 2, 'file.png')
         self.assertEqual({"status": "error", "message": 'Fill all fields'}, result)
 
     def test_for_empty_image_url_field(self):
         """Test for empty image url field"""
-        result = self.rec.create_recipe('biryani', '1 hour', ' 1 tbsp ', 'stir', 1, 2, '')
+        result = self.recipe.create_recipe('biryani', '1 hour', ' 1 tbsp ', 'stir', 1, 2, '')
         self.assertEqual({"status": "error", "message": 'No file chosen'}, result)
 
     # tests for update recipe function
-    def test_for_update_missing_recipe(self):
+    def test_for_update_non_existing_recipe(self):
         """Test for update missing recipe"""
-        self.rec.update_recipe(0, 'biryani', '1 hour', '1 tbsp', 'stir', 1, 2, 'file.png')
-        result = self.rec.update_recipe(1, 'biryani', '1 hour', '1 tbsp', 'stir', 1, 2, 'file.png')
+        image = os.path.dirname(os.path.realpath(__file__)) + "/test.jpg"
+        img = Image.open(image)
+        self.recipe.update_recipe(1, 'biryani', '1 hour', '1 tbsp', 'stir', 1, 2, img)
+        result = self.recipe.update_recipe(0, 'biryani', '1 hour', '1 tbsp', 'stir', 1, 2, img)
         self.assertEqual({"status": "error", "message": "Recipe does not exist"}, result)
 
     def test_for_delete_non_existing_recipe(self):
         """Test for delete non existing recipe"""
-        self.rec.delete_recipe(2)
-        result = self.rec.delete_recipe(0)
+        self.recipe.delete_recipe(2)
+        result = self.recipe.delete_recipe(0)
         self.assertEqual({"status": "error", "message": "Recipe does not exist"}, result)
 
     def test_for_missing_update_recipe(self):
         """Test for non-existing update recipe"""
-        self.rec.update_recipe(1, 'biryani', '1 hour', '1 tbsp', 'stir', 1, 2, 'file.png')
-        result = self.rec.update_recipe(0, 'biryani', '1 hour', '1 tbsp', 'stir', 1, 2, 'file.png')
+        image = os.path.dirname(os.path.realpath(__file__)) + "/test.jpg"
+        img = Image.open(image)
+        self.recipe.update_recipe(1, 'biryani', '1 hour', '1 tbsp', 'stir', 1, 2, img)
+        result = self.recipe.update_recipe(0, 'biryani', '1 hour', '1 tbsp', 'stir', 1, 2, img)
         self.assertEqual({"status": "error", "message": "Recipe does not exist"}, result)
-
-    def test_for_view_non_existing_recipes(self):
-        """Test for non-existing update category"""
-        self.rec.all_recipe()
-        result = self.rec.all_recipe()
-        self.assertEqual({"status": "error", "message": "Sorry, Recipe not available at the moment"}, result)
 
     def test_for_view_single_non_existing_recipes(self):
         """Test for non-existing update category"""
-        self.rec.single_recipe(0)
-        result = self.rec.single_recipe(2)
+        self.recipe.single_recipe(0)
+        result = self.recipe.single_recipe(2)
         self.assertEqual({"status": "error", "message": "Recipe does not exist"}, result)
+
+    def test_for_correct_creation_of_recipe(self):
+        """Test for correct recipe creation"""
+        image = os.path.dirname(os.path.realpath(__file__)) + "/test.jpg"
+        img = Image.open(image)
+        result = self.recipe.create_recipe('name', 'time', 'ingredients', 'directions', 1, 1, img)
+        self.assertEqual("success", result['status'])
+
+    def test_for_creation_of_recipe_with_invalid_characters(self):
+        """Test for recipe creation with invalid characters"""
+        image = os.path.dirname(os.path.realpath(__file__)) + "/test.jpg"
+        img = Image.open(image)
+        result = self.recipe.create_recipe(' ', ' ', ' ', ' ', 1, 1, img)
+        self.assertEqual({"message": "Invalid character input", "status": "error"}, result)
+
+    def test_adding_recipe_having_same_name_as_an_existing_recipe(self):
+        """Test for adding recipe with existing name"""
+        image = os.path.dirname(os.path.realpath(__file__)) + "/test.jpg"
+        img = Image.open(image)
+        self.recipe.create_recipe('name', 'time', 'ingredients', 'directions', 1, 1, img)
+        result = self.recipe.create_recipe('name', 'different time', 'other ingredients', 'other directions', 1, 1, img)
+        self.assertEqual({"message": "Recipe already exists", "status": "error"}, result)
+
+    def test_deletion_of_recipe(self):
+        """Test recipe deletion"""
+        image = os.path.dirname(os.path.realpath(__file__)) + "/test.jpg"
+        img = Image.open(image)
+        self.recipe.create_recipe('name', 'time', 'ingredients', 'directions', 1, 1, img)
+        result = self.recipe.delete_recipe(1)
+        self.assertEqual({'message': 'Recipe deleted successfully', 'status': 'success'}, result)
+
+    def test_deletion_of_non_existing_recipe(self):
+        """Test for delete non existing recipe"""
+        self.recipe.delete_recipe(2)
+        result = self.recipe.delete_recipe(0)
+        self.assertEqual({'message': 'Recipe does not exist', 'status': 'error'}, result)
+
+    def test_get_single_recipe(self):
+        """Test for get single recipe"""
+        image = os.path.dirname(os.path.realpath(__file__)) + "/test.jpg"
+        img = Image.open(image)
+        self.recipe.create_recipe('name', 'time', 'ingredients', 'directions', 1, 1, img)
+        self.assertEqual("success", self.recipe.single_recipe(1)['status'])
+
+    def test_get_non_existing_single_recipe(self):
+        """Test for get non existing single recipe"""
+        self.assertEqual({"message": "Recipe does not exist", "status": "error"},
+                         self.recipe.single_recipe(1))
+
+    def test_get_all_recipes(self):
+        """Test for get all recipes"""
+        image = os.path.dirname(os.path.realpath(__file__)) + "/test.jpg"
+        img = Image.open(image)
+        self.recipe.create_recipe('name', 'time', 'ingredients', 'directions', 1, 1, img)
+        self.recipe.create_recipe('name 1', 'time 1', 'ingredients 1', 'directions 1', 1, 1, img)
+        result = self.recipe.view_recipes(1, 1)
+        self.assertEqual(self.recipe.allrecipes, result)
 
 
 if __name__ == '__main__':
